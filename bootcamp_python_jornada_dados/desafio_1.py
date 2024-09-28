b688cd3eb47cb31c17361b62959486af3ee21e32
Thomas Raphael <thoraph96@gmail.com>
2024-09-27 23:41:40 -0300

    Adicionando arquivo do desafio 1

---
+++ bootcamp_python_jornada_dados/desafio_1.py
@@ -0,0 +1,17 @@
+# receber nome, salario e % do bonus
+# retorna o valor do bonus
+# 1000 + (salario * bonus)
+
+VALOR_BONUS_ANO = 1000
+
+# recebe informações do usuário
+nome = input("Qual seu nome? ")
+salario = float(input("Qual seu salário? "))
+perc_bonus = float(input("Qual o percentual do seu bônus? "))
+
+# calcula o bonus
+bonus_base = salario * perc_bonus
+bonus_final = VALOR_BONUS_ANO + bonus_base
+
+# retorna o resultado para o usuario
+print(f"Olá {nome}. Seu bônus será de {bonus_final:,.2f} reais.")
