c22630b8faaf9df41ec994c589afb3d158633882
Thomas Raphael <thoraph96@gmail.com>
2024-10-03 00:04:00 -0300

    Separando a captura das informações, calculo do bonus, criação e visualização do histórico em 4 funções

--- bootcamp_python_jornada_dados/desafio_4.py
+++ bootcamp_python_jornada_dados/desafio_4.py
@@ -1,20 +1,17 @@
-# receber nome, salario e % do bonus
-# retorna o valor do bonus
-# 1000 + (salario * bonus)
+# define lista de histórico de calculos
+historico_calculos: list = []
 
 VALOR_BONUS_ANO: float = 1000.00
 
-historico_calculos: list = []
+def capturar_informacoes() -> str | float:
+    # receber nome, salario e % do bonus
+    # retorna o valor do bonus
+    # 1000 + (salario * bonus)
 
-continuar: str = "s"
-
-while continuar == "s":
     # variáveis de controle do while
     nome_valido: bool = False
     salario_valido: bool = False
     bonus_valido: bool = False
-
-    calculo_atual: dict = {}
 
     # recebe informações do usuário
     while nome_valido is not True:
@@ -52,25 +49,33 @@ while continuar == "s":
         except ValueError:
             print("O multiplicador de bonus informado não é um número.")
 
+    return nome, salario, mult_bonus
+
+def calcular_bonus(salario: float, mult_bonus: float, valor_bonus_ano: float) -> float:
+
     # calcula o bonus
     bonus_base: float = salario * mult_bonus
-    bonus_final: float = VALOR_BONUS_ANO + bonus_base
+    bonus_final: float = valor_bonus_ano + bonus_base
 
+    return bonus_final
+
+def criar_historico(nome: str, salario: float, mult_bonus: float, valor_bonus_ano: float, bonus: float):
+    calculo_atual: dict = {}
+
+    # adiciona calculo atual no histórico
     calculo_atual = {
         "nome": nome,
         "salario": salario,
         "mult_bonus": mult_bonus,
-        "adicional_bonus_ano": VALOR_BONUS_ANO,
-        "bonus_final": bonus_final
+        "adicional_bonus_ano": valor_bonus_ano,
+        "bonus": bonus
     }
-    # adiciona calculo atual no histórico
+
+    # edita a lista de histórico
     historico_calculos.append(calculo_atual)
 
-    # imprime o resultado para o usuario
-    print(f"Olá {nome}. Seu bônus será de {bonus_final:,.2f} reais.")
-
-    mostrar_historico: str = input("Deseja ver o histórico de calculos? (s/n) ").lower()
-    if mostrar_historico == "s":
+def mostrar_historico(ver_historico):
+    if ver_historico == "s":
         for calculo in historico_calculos:
             i = historico_calculos.index(calculo)
             print('=' * 35)
@@ -79,7 +84,24 @@ while continuar == "s":
             print(f"- Salário: {calculo['salario']:,.2f}")
             print(f"- Multiplicador do Bônus: {calculo['mult_bonus']}")
             print(f"- Adicional de Bônus do Ano: {calculo['adicional_bonus_ano']:,.2f}")
-            print(f"- Bônus: {calculo['bonus_final']:,.2f}")
+            print(f"- Bônus: {calculo['bonus']:,.2f}")
+
+continuar: str = "s"
+while continuar == "s":
+
+    # chama a função que captura as informações
+    nome, salario, mult_bonus = capturar_informacoes()
+
+    # chama a função de calcular o bonus
+    bonus = calcular_bonus(salario, mult_bonus, VALOR_BONUS_ANO)
+
+    # retorna o resultado para o usuario
+    print(f"Olá {nome}. Seu bônus será de {bonus:,.2f} reais.")
+    
+    criar_historico(nome, salario, mult_bonus, VALOR_BONUS_ANO, bonus)
+
+    ver_historico: str = input("Deseja ver o histórico de calculos? (s/n) ").lower()
+    mostrar_historico(ver_historico)
 
     continuar = input("Você deseja fazer um novo calculo? (s/n) ").lower()
 
